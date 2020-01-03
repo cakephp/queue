97abaf94a8466616e56658bba7ded555ae667bea
ADmad <admad.coder@gmail.com>
2020-01-03 17:53:56 +0530

    Fix CS error

--- src/Job/MailerJob.php
+++ src/Job/MailerJob.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 namespace Queue\Job;
 
 use BadMethodCallException;
-use Cake\Mailer\Email;
 use Cake\Mailer\Exception\MissingMailerException;
 use Cake\Mailer\MailerAwareTrait;
 use Interop\Queue\Processor;
