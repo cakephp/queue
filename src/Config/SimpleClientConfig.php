8cc8ede294935d4adcc6fc51608eae5f31743f07
James McDonald <james@toggen.com.au>
2021-05-05 21:59:37 +1000

    Format changes

--- src/Config/SimpleClientConfig.php
+++ src/Config/SimpleClientConfig.php
@@ -23,16 +23,18 @@ class SimpleClientConfig
     /**
      * get
      * return the SimpleClientConfig array
+     * 
      * @return array
      */
     public function get(): array
     {
         return $this->simpleClientConfig;
     }
-    
+
     /**
      * getQueue
      * Return the queue name
+     *
      * @return string
      */
     public function getQueue(): string
@@ -56,12 +58,12 @@ class SimpleClientConfig
             'transport' => $config['url'],
             'client' =>
             [
-                'prefix'                   => 'enqueue',
-                'separator'                => '.',
-                'app_name'                 => 'app',
-                'router_topic'             => $this->queue,
-                'router_queue'             => $this->queue,
-                'default_queue'            => $this->queue,
+                'prefix' => 'enqueue',
+                'separator' => '.',
+                'app_name' => 'app',
+                'router_topic' => $this->queue,
+                'router_queue' => $this->queue,
+                'default_queue' => $this->queue,
             ],
             'extensions' => [
                 'signal_extension' => false,
