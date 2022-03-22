4e03b9d92d78bd7ad797e4333485fd2f242b5e9f
Alex Mayer <amayer5125@gmail.com>
2022-03-21 22:45:26 -0400

    Replace Incorrect FQCNs

--- tests/TestCase/Job/MailerJobTest.php
+++ tests/TestCase/Job/MailerJobTest.php
@@ -32,7 +32,7 @@ class MailerJobTest extends TestCase
      */
     protected $mailer;
     /**
-     * @var Cake\Queue\Job\MailerJob|\PHPUnit\Framework\MockObject\MockObject
+     * @var \Cake\Queue\Job\MailerJob|\PHPUnit\Framework\MockObject\MockObject
      */
     protected $job;
     protected $mailerConfig;
@@ -139,7 +139,7 @@ class MailerJobTest extends TestCase
     /**
      * Create a simple message for testing.
      *
-     * @return \Queue\Job\Message
+     * @return \Cake\Queue\Job\Message
      */
     protected function createMessage(): Message
     {
